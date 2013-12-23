0cef96a6f76a47eb1ac42729d6e0b6f3fccf5ac7
anyone-can-test <anyone.can.test@gmail.com>
2013-12-23 22:40:28 +0900

    jjj

--- src/main/java/org/jenkinsci/plugins/gcm/im/GcmImConnection.java
+++ src/main/java/org/jenkinsci/plugins/gcm/im/GcmImConnection.java
@@ -23,19 +23,24 @@ final class GcmImConnection implements IMConnection {
 
     @Override
     public void send(IMMessageTarget target, String text) throws IMException {
-	System.out.println("send: " + text);
-        //GcmMessageTarget gcmTarget = (GcmMessageTarget) target;
-        //String gcmToken = gcmTarget.getToken();
-
+    	System.out.println("send: " + text);
+        GcmMessageTarget gcmTarget = (GcmMessageTarget) target;
+        String buddyId = gcmTarget.getBuddyId();
+        
         //Result result = GcmManager.send(gcmToken, text);
         //checkResult(result, gcmTarget.getUserId());
 
-	try {
-		MyPeople.setAPIKEY("4200547f3ebd3d29061cc9ee456b266252de1271");
-		MyPeople.sendMessage("BU_lQaA7mh5f5bCAq_5lz7aOw00", text);
-	}catch (Exception e) {
-	
-	}
+		try {
+			MyPeople.setAPIKEY("4200547f3ebd3d29061cc9ee456b266252de1271");
+			//MyPeople.sendMessage("BU_lQaA7mh5f5bCAq_5lz7aOw00", text);
+			boolean bSuccess = MyPeople.sendMessage(buddyId, text);
+			
+			if (bSuccess == false) {
+				throw new IMException("sending error");
+			}
+		}catch (Exception e) {
+		
+		}
 
     }
 
